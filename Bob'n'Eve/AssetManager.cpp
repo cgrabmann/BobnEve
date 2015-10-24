52d851cb65bcd77391e0b34cc558e4f0e7c23c73
Radlwimmer Stefan <if14b052@technikum-wien.at>
2015-10-24 14:51:57 +0200

    AssetManager V1

--- Bob'n'Eve/AssetManager.cpp
+++ Bob'n'Eve/AssetManager.cpp
@@ -1,11 +1,126 @@
 #include "AssetManager.h"
 
-AssetManager::AssetManager()
+AssetManager::AssetManager(std::string rootDir) : textures_(), sounds_(), music_(), rootDir_(rootDir)
 {
-
+	textureDir_ = rootDir + "textures/";
+	soundDir_ = rootDir + "sounds/";
+	musicDir_ = rootDir + "music/";
 }
 
 AssetManager::~AssetManager()
 {
 
 }
+
+void AssetManager::LoadAll()
+{
+	LoadTextures();
+	LoadSounds();
+	LoadMusic();
+}
+
+void AssetManager::LoadTextures()
+{
+	std::vector<std::string> files = GetFilesInDir(textureDir_);
+
+	for (int i = 0; i < files.size(); i++)
+	{
+		LoadTextureByName(files[i]);
+	}
+}
+
+void AssetManager::LoadSounds()
+{
+	std::vector<std::string> files = GetFilesInDir(soundDir_);
+
+	for (int i = 0; i < files.size(); i++)
+	{
+		LoadSoundByName(files[i]);
+	}
+}
+
+void AssetManager::LoadMusic()
+{
+	std::vector<std::string> files = GetFilesInDir(musicDir_);
+
+	for (int i = 0; i < files.size(); i++)
+	{
+		LoadMusicByName(files[i]);
+	}
+}
+
+void AssetManager::LoadTextureByName(std::string name)
+{
+	std::string path = textureDir_ + name;
+
+	sf::Texture texture;
+	texture.loadFromFile(path);
+	texture.setSmooth(true);
+	textures_[name] = texture;
+}
+
+void AssetManager::LoadSoundByName(std::string name)
+{
+	std::string path = soundDir_ + name;
+
+	sf::SoundBuffer sound;
+	sound.loadFromFile(path);
+	sounds_[name] = sound;
+}
+
+void AssetManager::LoadMusicByName(std::string name)
+{
+	std::string path = musicDir_ + name;
+
+	sf::Music music;
+	music.openFromFile(path);
+	music_[name] = music;
+}
+
+std::vector<std::string> AssetManager::GetFilesInDir(std::string dir)
+{
+	std::vector<std::string> files;
+
+	return files;
+}
+
+sf::Sprite AssetManager::GetSpriteByName(std::string name)
+{
+	sf::Sprite sprite = sf::Sprite();
+
+	if (textures_.count(name) != 0)
+	{
+		sprite.setTexture(textures_[name]);
+	}
+
+	return sprite;
+}
+
+sf::Sound AssetManager::GetSoundByName(std::string name)
+{
+	sf::Sound sound = sf::Sound();
+
+	if (sounds_.count(name) != 0)
+	{
+		sound.setBuffer(sounds_[name]);
+	}
+
+	return sound;
+}
+
+sf::Music AssetManager::GetMusicByName(std::string name)
+{
+	sf::Music music = sf::Music();
+
+	if (music_.count(name) != 0)
+	{
+		music = music_[name];
+	}
+
+	return music;
+}
+
+
+
+
+
--- Bob'n'Eve/AssetManager.h
+++ Bob'n'Eve/AssetManager.h
@@ -1,13 +1,41 @@
 #pragma once
 
 #include <string>
+#include <SFML/Graphics/Texture.hpp>
+#include <SFML/Graphics/Sprite.hpp>
+#include <SFML/Audio/Sound.hpp>
+#include <SFML/Audio/SoundBuffer.hpp>
+#include <SFML/Audio/Music.hpp>
+#include <unordered_map>
 
 class AssetManager
 {
 public:
-	AssetManager();
+	AssetManager(std::string rootDir);
 	virtual ~AssetManager();
 
+	sf::Sprite GetSpriteByName(std::string name);
+	sf::Sound GetSoundByName(std::string name);
+	sf::Music GetMusicByName(std::string name);
+	
+	void LoadAll();
+	void LoadTextures();
+	void LoadSounds();
+	void LoadMusic();
+
+	void LoadTextureByName(std::string name);
+	void LoadSoundByName(std::string name);
+	void LoadMusicByName(std::string name);
+
 private:
+	std::vector<std::string> GetFilesInDir(std::string dir);
+
+	std::unordered_map<std::string, sf::Texture> textures_;
+	std::unordered_map<std::string, sf::SoundBuffer> sounds_;
+	std::unordered_map<std::string, sf::Music> music_;
+
 	std::string rootDir_;
+	std::string textureDir_;
+	std::string soundDir_;
+	std::string musicDir_;
 };
