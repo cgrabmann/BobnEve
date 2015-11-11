1af8cd119dcebf571ef11f03c82fdf4692c4caba
Radlwimmer Stefan <if14b052@technikum-wien.at>
2015-11-11 17:59:43 +0100

    Optimized GraphicsComponentFade

--- Bob'n'Eve/GraphicsComponentFade.cpp
+++ Bob'n'Eve/GraphicsComponentFade.cpp
@@ -6,7 +6,7 @@
 #include <SFML/include/SFML/Graphics/Sprite.hpp>
 
 
-GraphicsComponentFade::GraphicsComponentFade(std::vector<sf::Sprite*> sprites, int16_t msPerFrame) : GraphicsComponentAnimated(sprites, msPerFrame), lastIndex_(sprites.size() - 1), lastSprite_(sprites.at(lastIndex_)), alpha(255)
+GraphicsComponentFade::GraphicsComponentFade(std::vector<sf::Sprite*> sprites, int16_t msPerFrame) : GraphicsComponentAnimated(sprites, msPerFrame), lastIndex_(sprites.size() - 1), lastSprite_(sprites.at(lastIndex_)), alpha_(255), lastAlpha_(0)
 {
 }
 
@@ -25,7 +25,15 @@ void GraphicsComponentFade::Update(GameObject& object, int16_t ms)
 		lastSprite_ = sprite_;
 		sprite_ = sprites_.at(index_);
 	}
-	alpha = 255 * msCount_ / msPerFrame;
+
+	//alpha = msCount_ * 255.f / msPerFrame;
+	//alpha = pow(2, msCount_ * 8.f / msPerFrame) - 1;
+
+	int32_t base = 255 * 255;
+	float t = msCount_ * 1.f / msPerFrame;
+
+	alpha_ = sqrtf(base * t);
+	lastAlpha_ = sqrtf(base * (1 - t));
 }
 
 void GraphicsComponentFade::Draw(const GameObject& object, Renderer& renderer)
@@ -33,8 +41,8 @@ void GraphicsComponentFade::Draw(const GameObject& object, Renderer& renderer)
 	UpdateSprite(object, renderer, *sprite_);
 	UpdateSprite(object, renderer, *lastSprite_);
 
-	sprite_->setColor(sf::Color(255, 255, 255, alpha));
-	lastSprite_->setColor(sf::Color(255, 255, 255, 255 - alpha));
+	sprite_->setColor(sf::Color(255, 255, 255, alpha_));
+	lastSprite_->setColor(sf::Color(255, 255, 255, lastAlpha_));
 
 	//draw
 	renderer.GetTarget().draw(*sprite_);
